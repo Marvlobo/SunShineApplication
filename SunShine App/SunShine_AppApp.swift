a5a6ad2df3a7f7b303ecc4fd704213b32423533b
Marvin Lobo <marvinlobo11@gmail.com>
2022-10-24 00:59:55 -0400

    Initial Commit

---
+++ SunShine App/SunShine_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunShine_AppApp.swift
+//  SunShine App
+//
+//  Created by Marvin Lobo on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct SunShine_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
