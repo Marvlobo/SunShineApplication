c50e0e10d7328f2d1f54f29ec950e0ef7104389d
Marvin Lobo <marvinlobo11@gmail.com>
2022-11-07 19:45:53 -0500

    test for camera part 2

--- SunShine App/ContentView.swift
+++ SunShine App/ContentView.swift
@@ -8,81 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var capturedImage: UIImage? = nil
-    @State private var isCustomCameraViewPresented = false
-    var body: some View{
-        ZStack{
-            if capturedImage != nil{
-                Image(uiImage: capturedImage!)
-                    .resizable()
-                    .scaledToFit()
-                    .ignoresSafeArea()
-            }else{
-                Color(UIColor.systemBackground)
+    var body: some View {
+        TabView{
+            NavigationView{
+               navbar_home_view()
             }
-            VStack{
-                Spacer()
-                Button(action:{
-                    isCustomCameraViewPresented.toggle()
-                },label:{
-                    Image(systemName: "camera.fill")
-                        .font(.largeTitle)
-                        .padding()
-                        .background(Color.black)
-                        .foregroundColor(.white)
-                        .clipShape(Circle())
-                })
-                .padding(.bottom)
-                .sheet(isPresented: $isCustomCameraViewPresented, content:{
-                    CustomCameraView(capturedImage: $capturedImage)
-                })
+                .tabItem{
+                    Image("ic-navbar-home")
+                        .renderingMode(.template)
+               }
+            NavigationView{
+                navbar_public_view()
             }
-        }
-    }
-}
-    //var body: some View {
-        //TabView{
-            //NavigationView{
-               // navbar_home_view()
-            //}
-                //.tabItem{
-                    //Image("ic-navbar-home")
-                        //.renderingMode(.template)
-               // }
-            //NavigationView{
-                //navbar_public_view()
-            //}
-                //.tabItem{
-                    //Image("ic-navbar-public")
-                        //.renderingMode(.template)
-               // }
-            //NavigationView{
-                //navbar_sun_view()
-            //}
-            //.tabItem{
-                //Image("ic-navbar-sun")
-                    //.renderingMode(.template)
-            //}
-            //NavigationView{
-                //navbar_inbox_view_()
-            //}
-                //.tabItem{
-                    //Image("ic-navbar-inbox")
-                        //.renderingMode(.template)
-                //}
-            //NavigationView{
-                //navbar_profile_view()
-            //}
-                //.tabItem{
-                    //Image("ic-navbar-person")
-                        //.renderingMode(.template)
-               // }
+                .tabItem{
+                    Image("ic-navbar-public")
+                        .renderingMode(.template)
+               }
+            NavigationView{
+                navbar_sun_view()
+    
+            }
+            .tabItem{
+                Image("ic-navbar-sun")
+                    .renderingMode(.template)
+            }
+            NavigationView{
+                navbar_inbox_view_()
+            }
+                .tabItem{
+                    Image("ic-navbar-inbox")
+                        .renderingMode(.template)
+            }
+            NavigationView{
+                navbar_profile_view()
+            }
+                .tabItem{
+                    Image("ic-navbar-person")
+                        .renderingMode(.template)
+               }
             
-        //}
+        }
                 
-    //}
+    }
         
-//}
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
